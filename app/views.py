6f0e0372705a8cfd0d2a18120db40632de142bb1
vandana9989 <vandanaavula020@gmail.com>
2023-06-01 21:12:27 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def srujana(request):
+    return render(request,'srujana.html')
+
+def nani(request):
+    return render(request,'nani.html')
